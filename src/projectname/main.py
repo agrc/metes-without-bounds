2a747994692d7b9be9cccaf1f2d106ccd9453a35
steveoh <sgourley@utah.gov>
2025-10-01 13:18:33 -0600

    Initial commit

---
+++ src/projectname/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+"""
+a description of what this module does.
+this file is for testing linting...
+"""
+
+from . import version
+
+TEST = "test"
+
+
+def hello() -> str:
+    """a description of what this function does.
+
+    Returns:
+        str: returns "hi"
+    """
+    print("this is good")
+
+    print(
+        "this is a really, really, really, really, really, really, really, really, really, really, really, really,"
+        "really long line"
+    )
+
+    print(f"Version: {version.__version__}")
+
+    return "hi"
+
+
+if __name__ == "__main__":
+    #: the code that executes if you run the file or module directly
+    GREETING = hello()
